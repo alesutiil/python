faf42b4e73aca0280aa5ac4a42c277134d20cc2b
Alejandro Sutil <44579374+alesutiil@users.noreply.github.com>
2022-10-16 20:05:53 +0200

    [feat] Módulos

---
+++ mouredev/13_modules.py
@@ -0,0 +1,6 @@
+import my_module
+
+my_module.sum(5, 3, 1)
+
+# from my_module import sum
+# sum(5, 3, 1)
---
+++ mouredev/my_module.py
@@ -0,0 +1,3 @@
+def sum(valor_uno, valor_dos, valor_tres):
+    resultado = valor_uno + valor_dos + valor_tres
+    print(resultado)
